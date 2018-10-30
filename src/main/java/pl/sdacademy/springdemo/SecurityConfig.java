7fcdd9e95693255e70100fc7bb31ac1d270bd861
Mike Wojtyna <mike@buildmysoftware.pro>
2018-10-30 19:10:40 +0100

    Add custom form login config

--- src/main/java/pl/sdacademy/springdemo/SecurityConfig.java
+++ src/main/java/pl/sdacademy/springdemo/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.sdacademy.springdemo;
 
 import org.springframework.security.config.annotation.authentication.builders
 	.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration
 	.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration
@@ -9,6 +10,15 @@ import org.springframework.security.config.annotation.web.configuration
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+			authorizeRequests().antMatchers("/msg").authenticated
+			().and().formLogin().loginPage("/my-login")
+			.loginProcessingUrl("/login");
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws
 		Exception {
